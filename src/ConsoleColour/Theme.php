27ec3343689b43ba88e11420a933939426363656
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-03-23 21:27:33 +0200

    add Theme

---
+++ src/ConsoleColour/Theme.php
@@ -0,0 +1,109 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Tools\Reports\Formatters\Colour;
+
+use AlecRabbit\ConsoleColour;
+use AlecRabbit\ConsoleColour\Contracts\DefaultThemes;
+use AlecRabbit\Exception\ColorException;
+use AlecRabbit\Exception\InvalidStyleException;
+
+/**
+ * @method italic(string $text)
+ * @method dark(string $text)
+ * @method darkItalic(string $text)
+ * @method white(string $text)
+ * @method whiteBold(string $text)
+ * @method comment(string $text)
+ * @method yellow(string $text)
+ * @method error(string $text)
+ * @method red(string $text)
+ * @method green(string $text)
+ * @method info(string $text)
+ * @method underline(string $text)
+ * @method underlineBold(string $text)
+ * @method underlineItalic(string $text)
+ */
+class Theme implements DefaultThemes
+{
+    /** @var bool */
+    protected $doColorize;
+
+    /** @var ConsoleColour */
+    protected $color;
+
+    /**
+     * Themed constructor.
+     * @param bool $colorize
+     * @throws InvalidStyleException
+     */
+    public function __construct(bool $colorize = true)
+    {
+        $this->doColorize = $colorize;
+        $this->color = new ConsoleColour();
+        $this->setThemes();
+    }
+
+    /**
+     * @throws InvalidStyleException
+     *
+     */
+    protected function setThemes(): void
+    {
+        foreach (static::THEMES as $name => $styles) {
+            $this->color->addTheme($name, $styles);
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param array $arguments
+     * @return string
+     * @throws \Throwable
+     */
+    public function __call(string $name, array $arguments): string
+    {
+        $this->assertMethodName($name);
+        $this->assertArgs($name, $arguments);
+
+        return
+            $this->apply(static::THEMES[$name], $arguments[0]);
+    }
+
+    /**
+     * @param array|string $style
+     * @param string $text
+     * @return string
+     * @throws \Throwable
+     */
+    protected function apply($style, $text): string
+    {
+        try {
+            return
+                $this->doColorize ? $this->color->apply($style, $text) : $text;
+        } catch (ColorException $e) {
+            // do nothing
+        }
+        return $text;
+    }
+
+    /**
+     * @param string $name
+     * @param array $arguments
+     */
+    protected function assertArgs(string $name, array $arguments): void
+    {
+        if (1 !== \count($arguments)) {
+            throw new \ArgumentCountError('Method [' . $name . '] accepts only one argument.');
+        }
+    }
+
+    /**
+     * @param string $name
+     */
+    protected function assertMethodName(string $name): void
+    {
+        if (!\array_key_exists($name, static::THEMES)) {
+            throw new \BadMethodCallException('Unknown method call [' . $name . '] in [' . static::class . '].');
+        }
+    }
+}
