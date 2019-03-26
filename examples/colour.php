f7f88bfa8971187f92f3aac113a37d3d49c86964
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-03-26 20:11:08 +0200

    examples

--- examples/colour.php
+++ examples/colour.php
@@ -1,44 +1,66 @@
 <?php
 
 use AlecRabbit\ConsoleColour\ConsoleColour;
+use AlecRabbit\ConsoleColour\Exception\ColorException;
 
 const TEXT = '  *** Sample text ***  ';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $consoleColor = new ConsoleColour();
 
-echo 'Colors are supported: ' . ($consoleColor->isSupported() ? 'Yes' : 'No') . PHP_EOL;
-echo '256 colors are supported: ' . ($consoleColor->are256ColorsSupported() ? 'Yes' : 'No') . PHP_EOL . PHP_EOL;
-
 if ($consoleColor->isSupported()) {
+    echo 'Regular colors:' . PHP_EOL;
+
     foreach ($consoleColor->getPossibleStyles() as $style) {
-        echo str_pad("$style: ", 20) . $consoleColor->apply($style, TEXT) . PHP_EOL;
+        echo
+            $consoleColor->apply($style, TEXT) . ' ' . $style . PHP_EOL;
     }
 }
 
 echo PHP_EOL;
 
+/**
+ * @param ConsoleColour $consoleColor
+ * @param string $stylePrefix
+ * @return int
+ * @throws Throwable
+ * @throws ColorException
+ */
+function display(ConsoleColour $consoleColor, string $stylePrefix = ''): int
+{
+    for ($i = 0; $i <= 255; $i++) {
+        echo $consoleColor->apply($stylePrefix . 'color_' . $i, str_pad($i, 6, ' ', STR_PAD_BOTH));
+
+        if (($i + 1) % 16 === 0) {
+            echo PHP_EOL;
+        }
+    }
+    return $i;
+}
+
+/**
+ * @param $e
+ */
+function showException($e): void
+{
+    echo '[' . get_class($e) . ']' . $e->getMessage();
+}
+
 if ($consoleColor->are256ColorsSupported()) {
     echo '256 colors:' . PHP_EOL;
-
-    echo "Foreground colors:\n";
-    for ($i = 1; $i <= 255; $i++) {
-        echo $consoleColor->apply("color_$i", str_pad($i, 6, ' ', STR_PAD_BOTH));
-
-        if ($i % 15 === 0) {
-            echo PHP_EOL;
-        }
+    try {
+        echo 'Foreground colors:' . PHP_EOL;
+        display($consoleColor);
+        echo PHP_EOL . 'Background colors:' . PHP_EOL;
+        display($consoleColor, 'bg_');
+        echo PHP_EOL;
+    } catch (ColorException $e) {
+        showException($e);
+    } catch (Throwable $e) {
+        showException($e);
     }
-
-    echo "\nBackground colors:\n";
-
-    for ($i = 1; $i <= 255; $i++) {
-        echo $consoleColor->apply("bg_color_$i", str_pad($i, 6, ' ', STR_PAD_BOTH));
-
-        if ($i % 15 === 0) {
-            echo PHP_EOL;
-        }
-    }
-
-    echo PHP_EOL;
 }
+
+echo 'Colors are supported: ' . ($consoleColor->isSupported() ? 'Yes' : 'No') . PHP_EOL;
+echo '256 colors are supported: ' . ($consoleColor->are256ColorsSupported() ? 'Yes' : 'No') . PHP_EOL . PHP_EOL;
+
