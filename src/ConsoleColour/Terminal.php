543a0937e096acd7cb8921fe8338393297a78211
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-03-27 12:29:28 +0200

    refactor

--- src/ConsoleColour/Terminal.php
+++ src/ConsoleColour/Terminal.php
@@ -71,27 +71,9 @@ class Terminal
     protected static function initDimensions(): void
     {
         if (static::onWindows()) {
-            if ((false !== $term = \getenv('ANSICON')) &&
-                \preg_match('/^(\d+)x(\d+)(?: \((\d+)x(\d+)\))?$/', \trim($term), $matches)) {
-                // extract [w, H] from "wxh (WxH)"
-                // or [w, h] from "wxh"
-                static::$width = (int)$matches[1];
-                static::$height = isset($matches[4]) ? (int)$matches[4] : (int)$matches[2];
-            } elseif (null !== $dimensions = static::getConsoleMode()) {
-                // extract [w, h] from "wxh"
-                static::$width = (int)$dimensions[0];
-                static::$height = (int)$dimensions[1];
-            }
+            self::initDimensionsWindows();
         } elseif ($sttyString = static::getSttyColumns()) {
-            if (\preg_match('/rows.(\d+);.columns.(\d+);/i', $sttyString, $matches)) {
-                // extract [w, h] from "rows h; columns w;"
-                static::$width = (int)$matches[2];
-                static::$height = (int)$matches[1];
-            } elseif (\preg_match('/;.(\d+).rows;.(\d+).columns/i', $sttyString, $matches)) {
-                // extract [w, h] from "; h rows; w columns"
-                static::$width = (int)$matches[2];
-                static::$height = (int)$matches[1];
-            }
+            self::initDimensionsUnix($sttyString);
         }
     }
 
@@ -101,6 +83,21 @@ class Terminal
     protected static function onWindows(): bool
     {
         return '\\' === \DIRECTORY_SEPARATOR;
+    }
+
+    protected static function initDimensionsWindows(): void
+    {
+        if ((false !== $term = \getenv('ANSICON')) &&
+            \preg_match('/^(\d+)x(\d+)(?: \((\d+)x(\d+)\))?$/', \trim($term), $matches)) {
+            // extract [w, H] from "wxh (WxH)"
+            // or [w, h] from "wxh"
+            static::$width = (int)$matches[1];
+            static::$height = isset($matches[4]) ? (int)$matches[4] : (int)$matches[2];
+        } elseif (null !== $dimensions = static::getConsoleMode()) {
+            // extract [w, h] from "wxh"
+            static::$width = (int)$dimensions[0];
+            static::$height = (int)$dimensions[1];
+        }
     }
 
     /**
@@ -171,6 +168,22 @@ class Terminal
             return $info ?: null;
         }
         return null;
+    }
+
+    /**
+     * @param string $sttyString
+     */
+    protected static function initDimensionsUnix(string $sttyString): void
+    {
+        if (\preg_match('/rows.(\d+);.columns.(\d+);/i', $sttyString, $matches)) {
+            // extract [w, h] from "rows h; columns w;"
+            static::$width = (int)$matches[2];
+            static::$height = (int)$matches[1];
+        } elseif (\preg_match('/;.(\d+).rows;.(\d+).columns/i', $sttyString, $matches)) {
+            // extract [w, h] from "; h rows; w columns"
+            static::$width = (int)$matches[2];
+            static::$height = (int)$matches[1];
+        }
     }
 
     /**
