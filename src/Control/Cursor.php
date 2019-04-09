8d9edc80e4b351c38abdef6eabc4b3403dc3bb99
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-04-09 13:14:47 +0300

    up n down

--- src/Control/Cursor.php
+++ src/Control/Cursor.php
@@ -6,13 +6,45 @@ use AlecRabbit\ConsoleColour\ConsoleColor;
 
 class Cursor
 {
+    /**
+     * Show cursor sequence
+     *
+     * @return string
+     */
     public static function show(): string
     {
         return ConsoleColor::ESC_CHAR . '[?25h' . ConsoleColor::ESC_CHAR . '[?0c';
     }
 
+    /**
+     * Hide cursor sequence
+     *
+     * @return string
+     */
     public static function hide(): string
     {
         return ConsoleColor::ESC_CHAR . '[?25l';
+    }
+
+    /**
+     * Move up cursor sequence
+     *
+     * @param int $rows
+     * @return string
+     */
+    public static function up(int $rows = 1): string
+    {
+        return ConsoleColor::ESC_CHAR .  "[{$rows}A";
+    }
+
+    /**
+     * Move down cursor sequence
+     *
+     * @param int $rows
+     * @return string
+     */
+    public static function down(int $rows = 1): string
+    {
+        return ConsoleColor::ESC_CHAR .  "[{$rows}B";
     }
 }
