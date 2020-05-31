ab81b405e8c89828d55386da45921d590fbbd3af
Alec Rabbit <alec.therabbit.git@gmail.com>
2020-05-31 18:23:51 +0300

    ~rename

--- examples/new_version/hex_color_regex.php
+++ examples/new_version/hex_color_regex.php
@@ -6,7 +6,7 @@ const REGEXP = "/^#((?:[\da-f]{3}){1,2}|(?:[\da-f]{4}){1,2})$/i";
 
 require_once __DIR__ . '/bootstrap.php';
 
-$c = new \AlecRabbit\Console\Color\Closest();
+$c = new \AlecRabbit\Console\Color\ClosestColor256();
 
 $hex = '#f3e11';
 
