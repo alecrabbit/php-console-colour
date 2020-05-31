610e53095a765e497b81224064f7d1c4f940cada
Alec Rabbit <alec.therabbit.git@gmail.com>
2020-05-31 18:31:09 +0300

    ~wip

--- examples/new_version/font.php
+++ examples/new_version/font.php
@@ -9,78 +9,13 @@ use AlecRabbit\Tests\Helper;
 
 require_once __DIR__ . '/bootstrap.php';
 
-const TEXT = 'SoMe TeXt';
-const PAD_LENGTH = 52;
-const PAD_STR = ' ';
+for($i=0;$i<255;$i++ ){
 
-$c = new ClosestColor256();
-
-//$hexs = [
-//    '#ffffffaa',
-//    'fafafa',
-//    'e7e7e7',
-//    '8700d5',
-//    '870',
-//     0x870,
-//    'ad7',
-//    'f4f',
-//    'd56323',
-//    'd52363',
-//    'fff',
-//    '82376455',
-//    '887700',
-//];
-$hexs = [
-    '000000',
-    '800000',
-    '008000',
-    '808000',
-    '000080',
-    '800080',
-    '008080',
-    'c0c0c0',
-    '808080',
-    'ff0000',
-    '00ff00',
-    'ffff00',
-    '0000ff',
-    'ff00ff',
-    '00ffff',
-    'ffffff',
-];
-
-foreach ($hexs as $hex) {
-    $color256Number = $c->hex($hex);
-    $color256Text = sprintf(
-        "\x1b[38;5;%sm%s\x1b[0m",
-        $color256Number,
-        TEXT
+    $text = sprintf(
+        "\x1b[%sm%s\x1b[0m",
+        $i,
+        'TEXT'
     );
-    [$r, $g, $b, $a] = Converter::hex2rgba($hex);
-    $truecolorText = sprintf(
-        "\x1b[38;2;%sm%s\x1b[0m",
-        implode(';', [$r, $g, $b]),
-        TEXT
-    );
-    echo
-        sprintf(
-            'Hex: %s Normalized: %s',
-            str_pad('(' . \gettype($hex) . ')' . $hex, PAD_LENGTH, PAD_STR),
-            Converter::normalizeHex($hex)
-        ) . PHP_EOL;
-    echo
-        sprintf(
-            '%s %s %s',
-            str_pad(Helper::replaceEscape($truecolorText), PAD_LENGTH, PAD_STR),
-            str_pad('', 3, PAD_STR),
-            str_pad(Helper::replaceEscape($color256Text), PAD_LENGTH, PAD_STR)
-        ) . PHP_EOL;
-    echo
-        sprintf(
-            '%s                                 Closest: [%s] %s',
-            $truecolorText,
-            str_pad((string)$color256Number, 3, PAD_STR, STR_PAD_LEFT),
-            $color256Text
-        ) . PHP_EOL;
-    echo PHP_EOL;
+    echo $text . PHP_EOL;
 }
+    echo PHP_EOL;
