e73911df284911f320c3fafac191b44187d49f11
Alec Rabbit <alec.therabbit.git@gmail.com>
2020-05-31 18:23:56 +0300

    ~rename

--- examples/new_version/closest.php
+++ examples/new_version/closest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 
-use AlecRabbit\Console\Color\Closest;
+use AlecRabbit\Console\Color\ClosestColor256;
 use AlecRabbit\Console\Color\Converter;
 use AlecRabbit\Tests\Helper;
 
@@ -13,22 +13,40 @@ const TEXT = 'SoMe TeXt';
 const PAD_LENGTH = 52;
 const PAD_STR = ' ';
 
-$c = new Closest();
+$c = new ClosestColor256();
 
+//$hexs = [
+//    '#ffffffaa',
+//    'fafafa',
+//    'e7e7e7',
+//    '8700d5',
+//    '870',
+//     0x870,
+//    'ad7',
+//    'f4f',
+//    'd56323',
+//    'd52363',
+//    'fff',
+//    '82376455',
+//    '887700',
+//];
 $hexs = [
-    '#ffffffaa',
-    'fafafa',
-    'e7e7e7',
-    '8700d5',
-    '870',
-     0x870,
-    'ad7',
-    'f4f',
-    'd56323',
-    'd52363',
-    'fff',
-    '82376455',
-    '887700',
+    '000000',
+    '800000',
+    '008000',
+    '808000',
+    '000080',
+    '800080',
+    '008080',
+    'c0c0c0',
+    '808080',
+    'ff0000',
+    '00ff00',
+    'ffff00',
+    '0000ff',
+    'ff00ff',
+    '00ffff',
+    'ffffff',
 ];
 
 foreach ($hexs as $hex) {
